10e5d80fcc0d1d25a7172b7d514e19fee5ad36be
25Alan <romeroalan71@gmail.com>
2023-04-25 21:51:10 -0300

    S-13 | 220

--- product_app/lib/widgets/card_product.dart
+++ product_app/lib/widgets/card_product.dart
@@ -8,15 +8,25 @@ class CardProduct extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
       child: Container(
-        margin: EdgeInsets.only(top: 30, bottom: 50),
+        margin: const EdgeInsets.only(top: 30, bottom: 50),
         width: double.infinity,
         height: 400,
         decoration: _borderCard(),
         child: Stack(
           alignment: Alignment.bottomLeft,
-          children: [
+          children: const [
             _BackgroundCard(),
             _DetailsProduct(),
+            Positioned(
+              top: 0,
+              right: 0,
+              child: _PriceProduct(),
+            ),
+            Positioned(
+              top: 0,
+              left: 0,
+              child: _NotAvalaible(),
+            )
           ],
         ),
       ),
@@ -35,17 +45,112 @@ class CardProduct extends StatelessWidget {
           ]);
 }
 
+class _NotAvalaible extends StatelessWidget {
+  const _NotAvalaible();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 70,
+      decoration: BoxDecoration(
+        color: Colors.yellow[800],
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20),
+          bottomRight: Radius.circular(25),
+        ),
+      ),
+      child: const FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            'Not Avalaible',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _PriceProduct extends StatelessWidget {
+  const _PriceProduct();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      width: 100,
+      height: 70,
+      decoration: const BoxDecoration(
+        color: Colors.indigo,
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(20),
+          bottomLeft: Radius.circular(25),
+        ),
+      ),
+      child: const FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            '\$ 255.92',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class _DetailsProduct extends StatelessWidget {
   const _DetailsProduct();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 70,
-      color: Colors.indigo,
+    return Padding(
+      padding: const EdgeInsets.only(right: 60),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        width: double.infinity,
+        height: 70,
+        decoration: _boxDecorationsDetails(),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                'Disco 1',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(
+                'id - Disco',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              )
+            ]),
+      ),
     );
   }
+
+  BoxDecoration _boxDecorationsDetails() => const BoxDecoration(
+        color: Colors.indigo,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(20),
+          topRight: Radius.circular(25),
+        ),
+      );
 }
 
 class _BackgroundCard extends StatelessWidget {
@@ -58,7 +163,7 @@ class _BackgroundCard extends StatelessWidget {
       child: Container(
         width: double.infinity,
         height: 400,
-        child: FadeInImage(
+        child: const FadeInImage(
           placeholder: AssetImage('assets/jar-loading.gif'),
           image: NetworkImage('https://dummyimage.com/640x360/fff/aaa'),
           fit: BoxFit.cover,
