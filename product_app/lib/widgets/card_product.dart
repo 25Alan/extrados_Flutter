3dd3cc6e5402c47d76ebdbe351eb4ad922622251
25Alan <romeroalan71@gmail.com>
2023-04-25 21:19:18 -0300

    S-13 | 219

--- product_app/lib/widgets/card_product.dart
+++ product_app/lib/widgets/card_product.dart
@@ -10,8 +10,59 @@ class CardProduct extends StatelessWidget {
       child: Container(
         margin: EdgeInsets.only(top: 30, bottom: 50),
         width: double.infinity,
-        height: 10,
-        color: Colors.red,
+        height: 400,
+        decoration: _borderCard(),
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: [
+            _BackgroundCard(),
+            _DetailsProduct(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _borderCard() => BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 10,
+              offset: Offset(0, 5),
+            )
+          ]);
+}
+
+class _DetailsProduct extends StatelessWidget {
+  const _DetailsProduct();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      color: Colors.indigo,
+    );
+  }
+}
+
+class _BackgroundCard extends StatelessWidget {
+  const _BackgroundCard();
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: Container(
+        width: double.infinity,
+        height: 400,
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://dummyimage.com/640x360/fff/aaa'),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
