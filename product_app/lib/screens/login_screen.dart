ac5a15659351dfc937c1e2a2bac5d23117f3d5d4
25Alan <romeroalan71@gmail.com>
2023-05-25 19:23:57 -0300

    Fix

--- product_app/lib/screens/login_screen.dart
+++ product_app/lib/screens/login_screen.dart
@@ -63,86 +63,86 @@ class _FormLogin extends StatelessWidget {
     final formLogin = Provider.of<LoginProvider>(context);
 
     return Form(
-        key: formLogin.keyForm,
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        child: Column(
-          children: [
-            TextFormField(
-              autocorrect: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecorations.authInputDecorations(
-                hintText: 'example@email.com',
-                labelText: 'Email',
-                prefixIcon: Icons.email,
-              ),
-              onChanged: (value) => formLogin.email = value,
-              validator: (value) {
-                String pattern =
-                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-                RegExp regExp = RegExp(pattern);
+      key: formLogin.keyForm,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      child: Column(
+        children: [
+          TextFormField(
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecorations.authInputDecorations(
+              hintText: 'example@email.com',
+              labelText: 'Email',
+              prefixIcon: Icons.email,
+            ),
+            onChanged: (value) => formLogin.email = value,
+            validator: (value) {
+              String pattern =
+                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+              RegExp regExp = RegExp(pattern);
 
-                return regExp.hasMatch(value ?? '')
-                    ? null
-                    : 'The entered is not valid';
-              },
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            TextFormField(
-              autocorrect: false,
-              obscureText: true,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecorations.authInputDecorations(
-                hintText: '******',
-                labelText: 'Password',
-                prefixIcon: Icons.lock,
-              ),
-              onChanged: (value) => formLogin.password = value,
-              validator: (value) {
-                return (value != null && value.length >= 6)
-                    ? null
-                    : 'Password must contain 6 characters';
-              },
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            MaterialButton(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              disabledColor: Colors.grey,
-              elevation: 8,
-              color: Colors.deepPurple,
-              onPressed: formLogin.isLoading
+              return regExp.hasMatch(value ?? '')
                   ? null
-                  : () async {
-                      FocusScope.of(context).unfocus();
-                      final authS = Provider.of<ServiceAuth>(context,
-                          listen:
-                              false); // listen en false por que es mala practica escuchar dentro de un método
-                      formLogin.isLoading = true;
-                      final String? messageCreateUser = await authS.loginUser(
-                          formLogin.email, formLogin.password);
-                      if (messageCreateUser == null) {
-                        Navigator.pushReplacementNamed(context, 'home');
-                      } else {
-                        ServiceNotifiers.showSnackBar(
-                            '${messageCreateUser} [invalid data]');
-                        formLogin.isLoading = false;
-                      }
-                    },
-              child: Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                child: Text(
-                  formLogin.isLoading ? 'WAIT' : 'SIGN-IN',
-                  style: const TextStyle(color: Colors.white),
-                ),
+                  : 'The entered is not valid';
+            },
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          TextFormField(
+            autocorrect: false,
+            obscureText: true,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecorations.authInputDecorations(
+              hintText: '******',
+              labelText: 'Password',
+              prefixIcon: Icons.lock,
+            ),
+            onChanged: (value) => formLogin.password = value,
+            validator: (value) {
+              return (value != null && value.length >= 6)
+                  ? null
+                  : 'Password must contain 6 characters';
+            },
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          MaterialButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            disabledColor: Colors.grey,
+            elevation: 8,
+            color: Colors.deepPurple,
+            onPressed: formLogin.isLoading
+                ? null
+                : () async {
+                    FocusScope.of(context).unfocus();
+                    final authS = Provider.of<ServiceAuth>(context,
+                        listen:
+                            false); // listen en false por que es mala practica escuchar dentro de un método
+                    formLogin.isLoading = true;
+                    final String? messageCreateUser = await authS.loginUser(
+                        formLogin.email, formLogin.password);
+                    if (messageCreateUser == null) {
+                      Navigator.pushReplacementNamed(context, 'home');
+                    } else {
+                      ServiceNotifiers.showSnackBar(
+                          '${messageCreateUser} [invalid data]');
+                      formLogin.isLoading = false;
+                    }
+                  },
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+              child: Text(
+                formLogin.isLoading ? 'WAIT' : 'SIGN-IN',
+                style: const TextStyle(color: Colors.white),
               ),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
--- timer_bloc_app/lib/main.dart
+++ timer_bloc_app/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:timer_bloc_app/app.dart';
-import 'package:equatable/equatable.dart';
 
 void main() => runApp(const App());
